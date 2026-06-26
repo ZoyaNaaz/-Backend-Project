b9cce3cc00b27a9da93e1b885976278f6dc92e2f
Zoya <cse.zoya.2003@gmail.com>
2026-06-26 21:10:16 +0530

    Setup basic express server

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 import dotenv from "dotenv";
-
+import express from "express";
 dotenv.config({
   path: "./.env",
 });
 
-let myusername = process.env.username;
-console.log(myusername, process.env.database);
-console.log("Setup of an awesome project");
-console.log("hello");
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello express!");
+});
+
+app.get("/instagram", (req, res) => {
+  res.send("this is an instagram page")
+})
+
+app.listen(port, () => {
+  console.log(`app is listening on port http://localhost:${port}`);
+});
