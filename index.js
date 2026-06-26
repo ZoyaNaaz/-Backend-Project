863c1d5f59c1b86bac0477e8d84aec5efd76b412
Zoya <cse.zoya.2003@gmail.com>
2026-06-26 20:45:43 +0530

    Add dotenv package

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+import dotenv from "dotenv";
+
+dotenv.config({
+  path: "./.env",
+});
+
+let myusername = process.env.username;
+console.log(myusername, process.env.database);
 console.log("Setup of an awesome project");
 console.log("hello");
