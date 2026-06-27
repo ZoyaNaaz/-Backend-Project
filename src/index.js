0bdd00f5300e7fa20a319fa0639ba71e17fa9709
Zoya <cse.zoya.2003@gmail.com>
2026-06-27 22:47:42 +0530

    Config Database

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 import dotenv from "dotenv";
 import app from "./app.js";
+import connectDB from "./db/index.js";
 dotenv.config({
   path: "./.env",
 });
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`app is listening on port http://localhost:${port}`);
-});
+
+connectDB()
+  .then(() => {
+    app.listen(port, (req, res) => {
+      console.log(`App is listening on port http://localhost:${port}`);
+    });
+  })
+  .catch((err) => {
+    console.error(`MongoDB connection error: ${err}`);
+    process.exit(1);
+  });
