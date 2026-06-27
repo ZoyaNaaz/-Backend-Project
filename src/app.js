9cff4f7797596d44257000d28e1f82021fe2143d
Zoya <cse.zoya.2003@gmail.com>
2026-06-27 21:38:48 +0530

    Add ApiResponse and ApiError files

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
-
+// CORS Config
 app.use(
   cors({
     origin: process.env.CORS_ORIGIN?.split(",") || "http://localhost:5173",
