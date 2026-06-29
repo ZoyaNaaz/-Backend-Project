70f240a456a76d1c9963166e7142d365b77c028f
Zoya <cse.zoya.2003@gmail.com>
2026-06-29 21:45:27 +0530

    Add sendEmail functionality

--- src/utils/mail.js
+++ src/utils/mail.js
@@ -1,42 +1,81 @@
 import mailgen from "mailgen";
+import nodemailer from "nodemailer";
 
 const emailVerificationMailgenContent = (username, verificationUrl) => {
-    return {
-        body: {
-            name: username,
-            intro: "Welcome to our App! We're excited to have you on board.",
-            action: {
-                instructions: "To verify your email please click on the following button"
-                button: {
-                    color: "#22BC66",
-                    text: "Verify your email",
-                    link: verificationUrl
-                }
-            },
-            outro: "Need help, or have questions? Just reply to this email, we'd love to help."
-        }
-    }
-}
+  return {
+    body: {
+      name: username,
+      intro: "Welcome to our App! We're excited to have you on board.",
+      action: {
+        instructions:
+          "To verify your email please click on the following button",
+        button: {
+          color: "#22BC66",
+          text: "Verify your email",
+          link: verificationUrl,
+        },
+      },
+      outro:
+        "Need help, or have questions? `Just` reply to this email, we'd love to help.",
+    },
+  };
+};
 
 const forgotPasswordMailgenContent = (username, passwordResetUrl) => {
-    return {
-        body: {
-            name: username,
-            intro: "We got the request to reset your password",
-            action: {
-                instructions: "To reset password click on the following button or link"
-                button: {
-                    color: "#e23030",
-                    text: "Reset Your Password",
-                    link: passwordResetUrl
-                }
-            },
-            outro: "Need help, or have questions? Just reply to this email, we'd love to help."
-        }
-    }
-}
+  return {
+    body: {
+      name: username,
+      intro: "We got the request to reset your password",
+      action: {
+        instructions: "To reset password click on the following button or link",
+        button: {
+          color: "#e23030",
+          text: "Reset Your Password",
+          link: passwordResetUrl,
+        },
+      },
+      outro:
+        "Need help, or have questions? Just reply to this email, we'd love to help.",
+    },
+  };
+};
 
-export{
-    emailVerificationMailgenContent,
-    forgotPasswordMailgenContent
-}
+const sendEmail = async (options) => {
+  const mailGenerator = new Mailgen({
+    theme: "default",
+    product: {
+      name: "Task Manager",
+      link: "https://taskmanagerlink.com",
+    },
+  });
+
+  const emailTextual = mailGenerator.generatePlaintext(options.mailgenContent);
+  const emailHtml = mailGenerator.generate(options.mailgenContent);
+  const transporter = nodemailer.createTransport({
+    host: process.env.MAILTRAP_SMTP_HOST,
+    port: process.env.MAILTRAP_SMTP_PORT,
+    auth: {
+      user: process.env.MAILTRAP_SMTP_USER,
+      pass: process.env.MAILTRAP_SMTP_PASS,
+    },
+  });
+  const mail = {
+    from: "mail.taskmanager@example.com",
+    to: options.email,
+    subject: options.subject,
+    text: emailTextual,
+    html: emailHtml,
+  };
+
+  try {
+    await transporter.sendEmail(mail);
+  } catch (error) {
+    console.error("Error while sending mail:", error);
+  }
+};
+
+export {
+  emailVerificationMailgenContent,
+  forgotPasswordMailgenContent,
+  sendEmail,
+};
