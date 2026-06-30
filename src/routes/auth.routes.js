fa0f49e70f642a219be4bd19b34db83b84ea93f4
Zoya <cse.zoya.2003@gmail.com>
2026-06-30 22:03:42 +0530

    Login Validation

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,11 +1,14 @@
 import { Router } from "express";
 import { registerUser, login } from "../controllers/auth.controllers.js";
-import { userRegisterValidator } from "../validators/index.js";
+import {
+  userRegisterValidator,
+  userLoginValidator,
+  } from "../validators/index.js";
 import { validate } from "../middlewares/validator.middleware.js";
 
 const router = Router();
 
 router.route("/register").post(userRegisterValidator(), validate, registerUser);
-router.route("/login").post(login);
+router.route("/login").post(userLoginValidator, validate, login);
 
 export default router;
