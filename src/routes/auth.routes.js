206649d63bd444e181e169f978305c5c03fdf2ad
Zoya <cse.zoya.2003@gmail.com>
2026-07-02 22:17:58 +0530

    Add all auth routes

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -3,19 +3,51 @@ import {
   registerUser,
   login,
   logoutUser,
+  getCurrentUser,
+  verifyEmail,
+  resendEmailVerification,
+  forgotPasswordRequest,
+  changeCurrentPassword,
+  resetForgotPassword,
 } from "../controllers/auth.controllers.js";
 import {
   userRegisterValidator,
   userLoginValidator,
+  userChangeCurrentPasswordValidator,
+  userForgotPasswordValidator,
+  userResetPasswordValidator,
 } from "../validators/index.js";
 import { validate } from "../middlewares/validator.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
+import { verifyEmail } from "../controllers/auth.controllers.js";
 
 const router = Router();
 
+// unsecured routes
 router.route("/register").post(userRegisterValidator(), validate, registerUser);
 router.route("/login").post(userLoginValidator(), validate, login);
+router.route("/verify-email/:verificationToken").get(verifyEmail);
+router.route("/refresh-token").post(refreshAccessToken);
+router
+  .route("/forgot-passowrd")
+  .post(userForgotPasswordValidator(), validate, forgotPasswordRequest);
+router
+  .route("/reset-passowrd/:resetToken")
+  .post(userResetPasswordValidator(), validate, resetForgotPassword);
+
 // secure route
 router.route("/logout").post(verifyJWT, logoutUser);
+router.route("/current-user").post(verifyJWT, getCurrentUser);
+router
+  .route("/change-password")
+  .post(
+    verifyJWT,
+    userChangeCurrentPasswordValidator(),
+    validate,
+    changeCurrentPassword,
+  );
+router
+  .route("/resend-email-verification")
+  .post(verifyJWT, resendEmailVerification);
 
 export default router;
